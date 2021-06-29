f56c1762e71008998f1ab68d8022b6251b2c4c3a
DustinGentry <dustiumus@gmail.com>
2021-06-29 12:34:33 -0600

    added apm

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: 'f058fb712e2f46c4b2696bd8b9c8e4e4',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
 app.use(express.json());
 
+
 app.get('/', function(req, res){
+    rollbar.log('Hello world!')
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
